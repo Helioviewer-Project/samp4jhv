6ac5eff9b811a424094738f27ce9684ddfabe9ee
Laubeee <silvanlaube@hotmail.com>
2021-10-04 17:18:22 +0200

    recent example for RHESSI-to-JHV notebook, update hsi_fits2map including note to sunkit-instruments

--- ext/rhessi/hsi_fits2map.py
+++ ext/rhessi/hsi_fits2map.py
@@ -6,35 +6,48 @@ from sunpy.map import Map
 __all__ = ["hsi_fits2map"]
 
 
-def hsi_fits2map(url, crop_min=-1e10, crop_max=1e10):
+""" NOTE: as of february 2021 this functionality is part of sunkit-instruments v0.2 as imagecube2map()"""
+def hsi_fits2map(url):
+    """
+    Extracts single map images from a RHESSI flare image datacube. Currently
+    assumes input to be 4D.
+    This function is analogous to the `hsi_fits2map.pro` functionality available in SSW.
+    Parameters
+    ----------
+    rhessi_imagecube_file : `str`
+        Path or URL to image datacube .fits
+    Returns
+    -------
+    `dict` of `sunpy.map.MapSequence`
+        Each energy band has a list of maps where the index of the lists represent the time step
+    """
     f = read(url)
     header = f[0].header
+    
+    # remove those (non-standard) headers to avoid user warnings (they are 0 anyway)
     del header["CROTACN1"]
     del header["CROTACN2"]
     del header["CROTA"]
 
-    d_min = 1e10
-    d_max = -1e10
-    for t in range(len(f[1].data[0]["TIME_AXIS"])):
-        for e in range(len(f[1].data[0]["ENERGY_AXIS"])):
-            d_min = min(d_min, f[0].data[t][e].min())
-            d_max = max(d_max, f[0].data[t][e].max())
-    # crop
-    d_min = max(d_min, crop_min)
-    d_max = min(d_max, crop_max)
+    d_min = {}
+    d_max = {}
+    for e in range(len(f[1].data[0]["ENERGY_AXIS"])):
+        d_min[e] = 1e10
+        d_max[e] = -1e10
+        for t in range(len(f[1].data[0]["TIME_AXIS"])):
+            d_min[e] = min(d_min[e], f[0].data[t][e].min())
+            d_max[e] = max(d_max[e], f[0].data[t][e].max())
 
     maps = {}  # result dictionary
-    for e in f[1].data[0]["ENERGY_AXIS"].astype('int'):
-        maps[f"{e[0]}-{e[1]} keV"] = []  # init all layers (each energy band corresponds to a layer)
-
-    header["DATAMIN"] = d_min
-    header["DATAMAX"] = d_max
-    for t in range(len(f[1].data[0]["TIME_AXIS"])):
-        header["DATE_OBS"] = parse_time(f[1].data[0]["TIME_AXIS"][t][0], format='utime').to_value('isot')
-        header["DATE_END"] = parse_time(f[1].data[0]["TIME_AXIS"][t][1], format='utime').to_value('isot')
-        for e in range(len(f[1].data[0]["ENERGY_AXIS"])):
-            header["ENERGY_L"] = f[1].data[0]["ENERGY_AXIS"][e][0]
-            header["ENERGY_H"] = f[1].data[0]["ENERGY_AXIS"][e][1]
-            key = f"{int(header['ENERGY_L'])}-{int(header['ENERGY_H'])} keV"
+    for e in range(len(f[1].data[0]["ENERGY_AXIS"])):
+        header["ENERGY_L"] = f[1].data[0]["ENERGY_AXIS"][e][0]
+        header["ENERGY_H"] = f[1].data[0]["ENERGY_AXIS"][e][1]
+        header["DATAMIN"] = d_min[e]
+        header["DATAMAX"] = d_max[e]
+        key = f"{int(header['ENERGY_L'])}-{int(header['ENERGY_H'])} keV"
+        maps[key] = []
+        for t in range(len(f[1].data[0]["TIME_AXIS"])):
+            header["DATE_OBS"] = parse_time(f[1].data[0]["TIME_AXIS"][t][0], format='utime').to_value('isot')
+            header["DATE_END"] = parse_time(f[1].data[0]["TIME_AXIS"][t][1], format='utime').to_value('isot')
             maps[key].append(Map(f[0].data[t][e], header))  # extract image into sunpy.Map
     return maps
